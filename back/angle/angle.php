3908119d6e720feba034be37e2b527844efb3335
your_name_here <your_email@email.com>
2022-03-03 18:00:59 +0100

    hébergement majuscule

--- back/angle/angle.php
+++ back/angle/angle.php
@@ -15,7 +15,7 @@ require_once __DIR__ . '/../../util/ctrlSaisies.php';
 
 // Insertion classe Angle
 
-require_once __DIR__ . '/../../CLASS_CRUD/angle.class.php';
+require_once __DIR__ . '/../../class_crud/angle.class.php';
 
 // Instanciation de la classe angle
 
